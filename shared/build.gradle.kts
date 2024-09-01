96ca524747a1d0e8bd41a09edd8173dd7fa5ec3c
lluzalve <danielluz.alves@outlook.com>
2024-09-01 14:42:28 +1200

    Add KSP and annotations support

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,18 +1,22 @@
+import com.android.build.gradle.internal.ide.kmp.KotlinAndroidSourceSetMarker.Companion.android
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.ksp.kmp)
 }
 
 kotlin {
-    androidTarget {
+    android {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "1.8"
             }
         }
     }
-    
+
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -25,19 +29,52 @@ kotlin {
     }
 
     sourceSets {
-        commonMain.dependencies {
-            implementation(libs.koin)
-            implementation(compose.ui)
-            implementation(compose.runtime)
-            implementation(compose.components.resources)
-            implementation(compose.foundation)
-            implementation(compose.material3)
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.koin.annotations)
+            }
         }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.koin)
+                implementation(compose.ui)
+                implementation(compose.runtime)
+                implementation(compose.components.resources)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+                api(libs.koin.annotations)
+                implementation(libs.koin.core)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+            dependencies {
+                implementation(libs.koin.annotations)
+            }
         }
     }
+
+    sourceSets["commonMain"].kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
+    sourceSets["androidMain"].kotlin.srcDir("build/generated/ksp/android/androidMain/kotlin")
+
+    dependencies {
+        add("kspCommonMainMetadata", libs.koin.compiler)
+        add("kspAndroid", libs.koin.compiler)
+        add("kspIosX64", libs.koin.compiler)
+        add("kspIosArm64", libs.koin.compiler)
+        add("kspIosSimulatorArm64", libs.koin.compiler)
+
+    }
+
     task("testClasses")
+
 }
 
 android {
